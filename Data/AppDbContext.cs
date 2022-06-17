dc379fffeaaf805f0ee599ea5d014111e4040344
DBrzeski <brzeskirt@gmail.com>
2022-06-17 04:04:33 +0200

    <feat>Nuget,Database Connection:v0.0.2 -Installed Microsoft.EntityFrameworkCore v5.0.0 -Added AppDbContext -Added AppDbContext constructor -Added DbContext configuration in stratup

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using storeapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace storeapp.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Item { get; set; }
+    }
+}
