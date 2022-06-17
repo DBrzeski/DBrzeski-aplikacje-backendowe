1d88d82d7a2556f48f47350d9eed30ac4429a08b
DBrzeski <brzeskirt@gmail.com>
2022-06-17 03:50:55 +0200

    <feat>Models,Enums:v0.0.1 Added Items model for database Added ItemCategory Enum for storeing item category values

---
+++ Data/Enums/ItemCategory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace storeapp.Data.Enums
+{
+    public enum ItemCategory
+    {
+        [Description("Dog Food")]
+        Dog_Food,
+        [Description("Cat Food")]
+        Cat_Food,
+        [Description("Dog Toys")]
+        Dog_Toys,
+        [Description("Cat Food")]
+        Cat_Toys,
+        Hygene,
+        [Description("Dog Accessories")]
+        Dog_Accessories,
+        [Description("Cat Accessories")]
+        Cat_Accessories
+
+
+
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using storeapp.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace storeapp.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        //Basic information-----------------------------------
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        //Basic information-----------------------------------
+
+        public ItemCategory ItemCategory { get; set; }
+
+    }
+}
