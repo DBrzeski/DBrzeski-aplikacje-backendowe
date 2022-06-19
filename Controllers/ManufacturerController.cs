baa9405249b8cace303d1fc02c56a8546d1cb1e1
DBrzeski <brzeskirt@gmail.com>
2022-06-19 17:13:15 +0200

    <feat>Manufacturer Controller:v0.0.5 -Adding Manufacturer controller -Adding constructor in Manufacturer controller -Adding button forwarding to manufacturer

---
+++ Controllers/ManufacturerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using storeapp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace storeapp.Controllers
+{
+    public class ManufacturerController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ManufacturerController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Manufacturer.ToList();
+            return View();
+        }
+    }
+}
