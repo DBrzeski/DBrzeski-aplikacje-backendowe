7dc6009af6c9fa26f011019df6ac36aecbcdf329
DBrzeski <brzeskirt@gmail.com>
2022-06-19 16:47:22 +0200

    <feat>Database Connection,Models:v0.0.3 -Created Empty Database -Connected database in code -Added new model for Manufacturer Data

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +26,7 @@ namespace storeapp
         {
 
             //DbContext configuration
-            services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString")));
 
             services.AddControllersWithViews();
         }
