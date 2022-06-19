ff113bb98c7551a34f6b1145bff59627a2ef4571
DBrzeski <brzeskirt@gmail.com>
2022-06-19 17:21:57 +0200

    <feat>Item Controller:v0.0.6 -Adding Item Controller -Adding construktor in Item Controller

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using storeapp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace storeapp.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ItemController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allItems = await _context.Item.ToListAsync();
+            return View();
+        }
+    }
+}
