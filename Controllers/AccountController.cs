246a9143eaf74f7d714da0c927d9e219e1b83c9b
DBrzeski <brzeskirt@gmail.com>
2022-07-02 21:50:33 +0200

    <feat>Account,Identity,AccountController:v0.4.6 -Added Account controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using storeapp.Data;
+using storeapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace storeapp.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signinManager;
+        private readonly AppDbContext _context;
+
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signinManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
