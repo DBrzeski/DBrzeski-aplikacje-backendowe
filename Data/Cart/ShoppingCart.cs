7c859036df939bbbefa2b5a94464a167e47927bc
DBrzeski <brzeskirt@gmail.com>
2022-07-02 12:54:37 +0200

    <feat>Shopping,ShoppingCart:v0.3.1 -Added ShoppingCart Class -Added method for displaying all items in cart -Added method for calculating and returning total price for entire order

---
+++ Data/Cart/ShoppingCart.cs
@@ -0,0 +1,35 @@
+﻿using storeapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace storeapp.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItem { get; set; }
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItem ?? (ShoppingCartItem = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Item).ToList());
+        }
+        public double GetShoppingCartTotal()
+        {
+            var total = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Item.Price * n.Amount).Sum();
+            return total;
+        }
+
+
+
+    }
+}
